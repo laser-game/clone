121c3b1d85648f330d674cdf7f0125362bc5c0e2
wykys <vvykys@gmail.com>
2018-02-17 23:35:53 +0100

    added venv.sh

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Scripts for automating routines.
 
-
 ## Instalation:
 
 ```bash
@@ -13,7 +12,7 @@ git clone git@github.com:laser-game/utils.git
 
 ## List of scripts:
 
-### Clone
+### clone
 
 Clone is a bash script for creating a folder tree and cloning repositories to their correct location.
 
@@ -21,4 +20,15 @@ Clone is a bash script for creating a folder tree and cloning repositories to th
 ```bash
 cd utils
 ./clone.sh
+```
+
+### venv
+
+venv create the virtual enviroment from `requirements.txt` in sw module root folder.
+
+#### Use:
+```bash
+pwd
+# /home/wykys/projects/laser-game/sw/manager
+./../../utils/venv.sh
 ```
---
+++ venv.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# create and install python enviroment
+# wykys 2018
+
+if [[ $(pwd) == */laser-game/sw/manager ]] || \
+   [[ $(pwd) == */laser-game/sw/driver ]]  || \
+   [[ $(pwd) == */laser-game/sw/bridge-api ]]; then
+    echo "folder id ok"
+else
+    echo "Error: You must go to the root folder of the sw module!"
+    exit -1
+fi
+
+echo "remove old venv"
+rm .venv -rf
+
+echo "create new venv"
+python3 -m venv .venv
+
+echo "activate venv"
+. .venv/bin/activate
+
+echo "upgrade pip"
+pip install --upgrade pip
+
+echo "install modules from requirements"
+pip install -r requirements.txt
+
+echo "installed modules"
+pip freeze
