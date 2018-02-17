74328138d2ccfc2a548edfa7723b708d1aec5212
wykys <vvykys@gmail.com>
2018-02-17 21:45:00 +0100

    changed from bitbucket to github

--- clone.sh
+++ clone.sh
@@ -6,6 +6,6 @@
 mkdir sw hw
 
 cd sw
-git clone git@bitbucket.org:laser-game/manager.git
-git clone git@bitbucket.org:laser-game/driver.git
-git clone git@bitbucket.org:laser-game/bridge-api.git
+git clone git@github.com:laser-game/manager.git
+git clone git@github.com:laser-game/driver.git
+git clone git@github.com:laser-game/bridge-api.git
